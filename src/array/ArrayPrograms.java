c0f1c5c67333c7c9e7166dd0d62f53a436f371f4
Vinothkumar <vinothvino0518@gmail.com>
2026-03-10 13:18:04 +0530

    DSA problems Practice

--- src/array/ArrayPrograms.java
+++ src/array/ArrayPrograms.java
@@ -1,4 +1,13 @@
 package array;
 
 public class ArrayPrograms {
+    public static void main(String[] args) {
+        int[] newArray = new int[6];
+
+        int[] mcq = {100,99,76,45};
+
+        for(int i=0;i<mcq.length;i++){
+            System.out.println(mcq[i]);
+        }
+    }
 }
