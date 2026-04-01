87846ee53c9c0bd96220f84cbbd3672e8f1644dd
Vinothkumar <vinothvino0518@gmail.com>
2026-04-01 13:42:28 +0530

    twosum

--- src/array/TwoSum.java
+++ src/array/TwoSum.java
@@ -1,4 +1,22 @@
 package array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
-}
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int sum = target - nums[i];
+            if (map.containsKey(sum)) {
+                System.out.println(map.get(sum)+ " " + i);
+                System.out.println(sum + " " + nums[i]);
+                return;
+            }
+            map.put(nums[i],i);
+        }
+    }
+    }
